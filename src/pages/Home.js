3bc898d733a2b05cf98f036b8a7d465dd8bdae4f
Queenie Wu <32552540+queeniwu@users.noreply.github.com>
2020-12-04 12:57:19 -0500

    added elements to landing page, still needs to define where it is tho.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,52 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import letitout from "../assets/Letitout.svg";
+import face from "../assets/face.svg";
+import iaq from "../assets/IAQ.svg";
+import e5 from "../assets/pic.png";
+
+
+const Face = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const E5 = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  z-index: 0;
+`;
+
+
+const IAQ = styled.div`
+  display: block;
+  position: absolute;
+  height: 10vh;
+  width: 20vw;
+  margin: 35vw;
+`;
 
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
-
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
   align-items: flex-end;
-  background-color: #0F0F0F;
+  background-color: #000000;
 `;
 
-const PrimaryCTA = styled.button`
-    cursor: pointer;
+const PrimaryCTA = styled.div`
     height: 10vh;
     width: 20vw;
     margin: 10vw;
-    font-size: 38px;
+`;
 
+const Link = styled.img`
+    cursor: pointer;
     &:hover {
-        background-color: #EAFCFE;
+
     }
 `;
 
@@ -29,9 +54,18 @@ export function Home() {
     const history = useHistory();
     return (
         <Container>
-            <PrimaryCTA onClick={() => {history.push("/map")}}>
-                let it out →
-            </PrimaryCTA>
+          <Face>
+            <img src={face} alt="Face" />;
+          </Face>,
+          <IAQ>
+            <Link src={iaq} alt="Infrequently Asked Questions" />;
+          </IAQ>,
+          <PrimaryCTA>
+            <Link src={letitout} alt="Entry Link" onClick={() => {history.push("/map")}}/>
+          </PrimaryCTA>,
+          <E5>
+          <img src={e5} alt="E5" />;
+          </E5>
         </Container>
     );
 };
