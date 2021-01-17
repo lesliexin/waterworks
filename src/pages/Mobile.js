ae76a4716900979fb7dbc6de010753449c53b378
Queenie Wu <32552540+queeniwu@users.noreply.github.com>
2021-01-17 13:19:11 -0800

    styled mobile page

--- src/pages/Mobile.js
+++ src/pages/Mobile.js
@@ -15,16 +15,46 @@ const Container = styled.div`
 
 const Title = styled.div`
   position: absolute;
-  top: 680px;
-  left: 1140px;
+  width: 80vw;
+  max-width: 500px;
+  top: 25vh;
+  left: 24px;
   z-index: 2;
   font-family: 'presicav', sans-serif;
   font-weight: 900;
-  font-size: 42px;
+  font-size: 30px;
   letter-spacing: 0px;
-  color: #ffffff;
+  color: #14161b;
+  text-shadow: 0.6px 0.6px 0px  #fff, -0.6px -0.6px 0px  #fff, 0.6px -0.6px 0px  #fff, -0.6px 0.6px 0px  #fff;
 `;
 
+
+const Body = styled.div`
+  position: absolute;
+  width: 80vw;
+  left: 24px;
+  top: 55vh;
+  padding-top: 12px;
+  z-index: 1;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 14px;
+  font-weight: 600;
+  color: #fff;
+`;
+
+
+const Link = styled.div`
+    cursor: cell;
+    padding-top: 8px;
+    font-weight: 600;
+    font-style: oblique;
+    text-decoration: underline;
+    color: #F2874F;
+    &:hover {
+      border: 1px solid rgb(127, 0, 0, 0);
+      opacity: 80%;
+    }
+`;
 
 export function Mobile() {
       const history = useHistory();
@@ -33,6 +63,12 @@ export function Mobile() {
     <Title>
     we're not great at responsive design.
     </Title>
+    <Body>
+    Mind using a laptop or desktop?
+    <Link>
+    or, read more about this project
+    </Link>
+    </Body>
     </Container>
   );
 };
