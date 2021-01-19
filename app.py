608a5e6841953aa5f869bbd563f3cd9c8bf36340
lesliexin <leslie.xin@hotmail.com>
2021-01-19 17:37:48 -0500

    heroku config

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import os
 from flask import Flask
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 
 filename = os.path.join(app.static_folder, 'data.json')
 
